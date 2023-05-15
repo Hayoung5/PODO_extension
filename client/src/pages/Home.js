f08185c8c9340cbd1c9da6e8e71d721d3dd47704
hayoung5 <dalja6309@gmail.com>
2023-05-15 20:50:10 +0900

    metamask test

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -56,23 +56,24 @@ const Button_Mypage = styled(Button)`
     font-size: 18px;
 `;
 
-// // 이벤트 수신 함수
-// function handleMetaMaskEvent(event) {
-// 	console.log("message!!");
-// 	if (event.data && event.data.method === 'eth_sendTransaction') {
-// 		console.log("yes");
-// 		console.log(event.data);
-// 		const rawTransaction = event.data.params[0];
-// 		console.log(rawTransaction);
-// 	}
-//   }
+// 이벤트 수신 함수
+const handleMetaMaskEvent = (event) => {
+	console.log("message!!");
+	if (event.data && event.data.method === 'eth_sendTransaction') {
+		console.log("yes");
+		console.log(event.data);
+		const rawTransaction = event.data.params[0];
+		console.log(rawTransaction);
+	}
+  }
   
 const Home = ({setInputValue, setIsURL}) => {
 	const navigate = useNavigate();
 
 	useEffect(() => {
 		// 메타마스크 이벤트 수신 대기
-		// window.addEventListener('message', handleMetaMaskEvent);
+		console.log();
+		window.addEventListener('message', handleMetaMaskEvent);
 	}, []);
 
 	const handleKeyPress = (event) => {
