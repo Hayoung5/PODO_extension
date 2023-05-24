03d4095d0d59c6e0f50d52b6ea351d198b1e174b
hayoung5 <dalja6309@gmail.com>
2023-05-24 21:40:02 +0900

    change server url

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    apiBaseUrl: 'http://localhost:3000'
+    // apiBaseUrl: 'http://localhost:3000'
+    apiBaseUrl: 'http://34.64.162.66:30010'
   };
   
   export default config;
